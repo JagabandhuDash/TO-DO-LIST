131b640a272bf01fad72ef749022eb03c4f5c507
JagabandhuDash <jagabandhudash710@gmail.com>
2026-07-07 09:11:15 +0530

    baisc styling

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
+function formSubmit() {
+    event.preventDefault()
+    let data = getData()
+    setData(data)()
+
+}
+
 function getData(){
-let data = document.getElementsByTagName("input").value
+let data = document.getElementById("text-box")
+let obj = {}
+obj["text"] = data.value 
+return obj;
+}
+
+function setData(data){
+let table = document.getElementsByTagName("tbody")[0]
+let row = table.insertRow(table.length)
+
+text = row.insertCell(0)
+text.innerHTML = data.text 
+
+button = row.insertCell(1)
+button.innerHTML = `<button id="delete-button"><i class="fa-solid fa-trash"></i></button>`
 }
