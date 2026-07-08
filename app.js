193792e58a0f9d59eb7520946b6882adf3cb84d4
JagabandhuDash <jagabandhudash710@gmail.com>
2026-07-08 12:14:56 +0530

    Upgrading

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 function formSubmit() {
     event.preventDefault()
     let data = getData()
-    setData(data)()
+    setData(data)
+    resetData()
 
 }
 
@@ -16,9 +17,23 @@ function setData(data){
 let table = document.getElementsByTagName("tbody")[0]
 let row = table.insertRow(table.length)
 
+
+
 text = row.insertCell(0)
 text.innerHTML = data.text 
 
 button = row.insertCell(1)
-button.innerHTML = `<button id="delete-button"><i class="fa-solid fa-trash"></i></button>`
+button.innerHTML = `<button id="delete-button" onclick="deleteData(this)"><i class="fa-solid fa-trash"></i></button>`
+text.style.width="500px"
+table.style.borderRadius="10px"
+}
+
+function deleteData(data){
+ row = data.parentElement.parentElement
+ document.getElementById("to-do-list").deleteRow(row.rowIndex)
+}
+
+function resetData() {
+document.getElementById("text-box").value=""
+
 }
